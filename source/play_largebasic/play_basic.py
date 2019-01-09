33efe1be5ade84816587e9314e263a39a52aa69d
YutaroKiuchi <kikuchi.yutaro.s1@dc.tohoku.ac.jp>
2019-01-09 15:11:54 +0900

    Able to save demonstration

--- source/play_largebasic/play_basic.py
+++ source/play_largebasic/play_basic.py
@@ -101,10 +101,74 @@ if __name__ == "__main__":
 
         s_row = game.get_screen_buff()
         s,s_center = preprocess(s_row)
+        label_enemy = [game.get_enemy_x(), game.get_enemy_y(), game.get_enemy_w(), game.get_enemy_h()]
         reward,reward_detail = game.advance_action(step,4)
         action = game.get_last_action()
-        step += 1
+        print(action)
 
-        print(reward_detail)
-        # print('%5.2f %5.2f %5.2f %5.2f %5.2f '%(reward,game.get_frag_count(), game.get_death_count(), game.get_pos_x(), game.get_pos_y()))
+        if not action.count(1.0) == 0:
+            step += 1
+            if SAVE_DEMO == True and not game.is_episode_finished():
+                log_state.append(s)
+                log_state_center.append(s_center)
+                log_action.append(action)
+                log_frag.append(game.get_frag_count())
+                log_death.append(game.get_death_count())
+                log_health.append(game.get_health())
+                log_ammo.append(game.get_ammo())
+                log_posx.append(game.get_pos_x())
+                log_posy.append(game.get_pos_y())
+                log_enemy_x.append(label_enemy[0])
+                log_enemy_y.append(label_enemy[1])
+                log_enemy_w.append(label_enemy[2])
+                log_enemy_h.append(label_enemy[3])
+                log_hitcount.append(game.get_hitcount())
+                log_damagecount.append(game.get_damagecount())
+            elif SAVE_DEMO == True and game.is_episode_finished():
+                    log_state.append(s)
+                    log_state_center.append(s_center)
+                    log_action.append(action)
+                    log_frag.append(log_frag[-1])
+                    log_death.append(log_death[-1])
+                    log_health.append(log_health[-1])
+                    log_ammo.append(log_ammo[-1])
+                    log_posx.append(log_posx[-1])
+                    log_posy.append(log_posy[-1])
+                    log_enemy_x.append(label_enemy[0])
+                    log_enemy_y.append(label_enemy[1])
+                    log_enemy_w.append(label_enemy[2])
+                    log_enemy_h.append(label_enemy[3])
+                    log_hitcount.append(log_hitcount[-1])
+                    log_damagecount.append(log_damagecount[-1])
+            else:
+                pass
+
+        #print(reward_detail)
+        print('%5.2f %5.2f %5.2f %5.2f %5.2f '%(reward,game.get_frag_count(), game.get_death_count(), game.get_pos_x(), game.get_pos_y()))
     game.close()
+
+    if SAVE_DEMO == True:
+      
+        with h5py.File(DEMO_PATH, "r+") as f:
+            groups = list(f.keys())
+            groups = [int(s) for s in groups]
+            groups.sort()
+            print(groups)
+            print("n_step:",len(log_action))
+            new_group = str(groups[-1] + 1)
+            print("No.",new_group,"'s data is saving")
+            f.create_dataset(new_group+"/states", data=log_state)
+            f.create_dataset(new_group+"/states_center", data=log_state_center)
+            f.create_dataset(new_group+"/action", data=log_action)
+            f.create_dataset(new_group+"/frag",data=log_frag)
+            f.create_dataset(new_group+"/death",data=log_death)
+            f.create_dataset(new_group+"/health",data=log_health)
+            f.create_dataset(new_group+"/ammo",data=log_ammo)
+            f.create_dataset(new_group+"/posx",data=log_posx)
+            f.create_dataset(new_group+"/posy",data=log_posy)
+            f.create_dataset(new_group+"/enemy_x",data=log_enemy_x)
+            f.create_dataset(new_group+"/enemy_y", data=log_enemy_y)
+            f.create_dataset(new_group+"/enemy_w", data=log_enemy_w)
+            f.create_dataset(new_group+"/enemy_h", data=log_enemy_h)
+            f.create_dataset(new_group+"/hitcount", data=log_hitcount)
+            f.create_dataset(new_group+"/damagecount", data=log_damagecount)
